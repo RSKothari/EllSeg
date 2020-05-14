32c4c42ad1e217a7708786cc6f726ff8db1a5d74
RSKothari <rsk3900@rit.edu>
2020-05-14 07:04:16 -0400

    Added multitest strategies

--- runRC_testCode.sh
+++ runRC_testCode.sh
@@ -10,23 +10,27 @@ spack env activate riteyes4
 model="ritnet_v1"
 declare -a curObj_list=("NVGaze" "PupilNet" "OpenEDS" "Fuhl" "riteyes_general" "LPW")
 declare -a batchsize_list=("16" "16" "16" "16" "16" "16")
-declare -a selfCorr_list=("0")
+declare -a selfCorr_list=("0" "1")
 declare -a disentangle_list=("0")
+declare -a tests=("baseline" "random" "leaveoneout")
 
-for i in "${!curObj_list[@]}"
-do
-    for selfCorr in "${selfCorr_list[@]}"
+for test_mode in "${tests[@]}"
     do
-        for disentangle in "${disentangle_list[@]}"
+    for i in "${!curObj_list[@]}"
+    do
+        for selfCorr in "${selfCorr_list[@]}"
         do
-            batchsize=${batchsize_list[i]}
-            baseJobName="RC_e2e_${model}_${curObj_list[i]}_${selfCorr}_${disentangle}"
-            str="#!/bin/bash\npython3 train.py --path2data=${path2ds} --expname=${baseJobName} "
-            str+="--curObj=${curObj_list[i]} --batchsize=${batchsize} --workers=${workers} --prec=32 --epochs=${epochs} "
-            str+="--disp=0 --overfit=20 --lr=${lr} --selfCorr=${selfCorr} --disentangle=${disentangle} --model=${model}"
-            echo $str
-            echo -e $str > command.lock
-            sbatch -J ${baseJobName} -o "rc_log/system_test/${baseJobName}.o" -e "rc_log/system_test/${baseJobName}.e" --mem=16G --cpus-per-task=9 -p debug -A riteyes --gres=gpu:p4:2 -t 0-5:0:0 command.lock
+            for disentangle in "${disentangle_list[@]}"
+            do
+                batchsize=${batchsize_list[i]}
+                baseJobName="RC_e2e_${test_mode_${model}_${curObj_list[i]}_${selfCorr}_${disentangle}"
+                str="#!/bin/bash\npython3 train.py --path2data=${path2ds} --expname=${baseJobName} --test_mode=${test_mode}"
+                str+="--curObj=${curObj_list[i]} --batchsize=${batchsize} --workers=${workers} --prec=32 --epochs=${epochs} "
+                str+="--disp=0 --overfit=20 --lr=${lr} --selfCorr=${selfCorr} --disentangle=${disentangle} --model=${model}"
+                echo $str
+                echo -e $str > command.lock
+                sbatch -J ${baseJobName} -o "rc_log/${test_mode}/${baseJobName}.o" -e "rc_log/${test_mode}/${baseJobName}.e" --mem=16G --cpus-per-task=9 -p debug -A riteyes --gres=gpu:p4:2 -t 0-5:0:0 command.lock
+            done
         done
     done
 done
--- train.py
+++ train.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     os.makedirs(path2checkpoint, exist_ok=True)
     os.makedirs(path2writer, exist_ok=True)
 
-    f = open(os.path.join('curObjects','baseline','cond_'+str(args.curObj)+'.pkl'), 'rb')
+    f = open(os.path.join('curObjects',args.test_cond,'cond_'+str(args.curObj)+'.pkl'), 'rb')
 
     trainObj, validObj, _ = pickle.load(f)
     trainObj.path2data = os.path.join(args.path2data, 'Dataset', 'All')
