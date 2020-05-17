c0782c9401a5541e976028f0eb10439517f40110
RSKothari <rsk3900@rit.edu>
2020-05-17 15:06:20 -0400

    Added V5 in ModelSummary

--- modelSummary.py
+++ modelSummary.py
@@ -10,11 +10,13 @@ from models.RITnet_v1 import DenseNet2D as DN_v1
 from models.RITnet_v2 import DenseNet2D as DN_v2
 from models.RITnet_v3 import DenseNet2D as DN_v3
 from models.RITnet_v4 import DenseNet2D as DN_v4
+from models.RITnet_v5 import DenseNet2D as DN_v5
 from models.deepvog_pytorch import DeepVOG_pytorch
 
 model_dict = {}
 model_dict['ritnet_v1'] = DN_v1()
-model_dict['ritnet_v2'] = DN_v2() # Iris and Pupil scheme does not work well
+model_dict['ritnet_v2'] = DN_v2()
 model_dict['ritnet_v3'] = DN_v3()
 model_dict['ritnet_v4'] = DN_v4()
+model_dict['ritnet_v5'] = DN_v5()
 model_dict['deepvog'] = DeepVOG_pytorch()
